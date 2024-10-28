80d97fd3dfe9f3d64e948e2e9ac1f3c843e29350
Choi Hyuk Tae <40112123+htchoi1006@users.noreply.github.com>
2024-10-28 20:07:23 +0900

    [Silver II] Title: 참외밭, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,30 +1,22 @@
 n = int(input())
-field = [list(map(int, input().split())) for _ in range(6)]
-# field = [[4, 50], [2, 160], [3, 30], [1, 60], [3, 20], [1, 100]]
-# field.sort()
-# print(field)
-
+arr = [list(map(int, input().split())) for _ in range(6)]
 
 row, col = 0, 0
-big, small = 0, 0
-idx_row = 0
-idx_col = 0
+idx_row, idx_col = 0, 0
 
-for i in range(len(field)):    
-    if(field[i][0] == 1 or field[i][0] == 2):
-        if(field[i][1] > row):
-            row = field[i][1]
+for i in range(6):
+    if arr[i][0] == 1 or arr[i][0] == 2: 
+        if arr[i][1] > row:
+            row = arr[i][1]
             idx_row = i
-            
-    elif(field[i][0] == 3 or field[i][0] == 4):
-        if(field[i][1] > col):
-            col = field[i][1]
+        
+    elif arr[i][0] == 3 or arr[i][0] == 4: 
+        if arr[i][1] > col:
+            col = arr[i][1]
             idx_col = i
-            
-# print(row, col)
-big = row*col
-
-small = abs(field[(idx_row+1)%6][1] - field[(idx_row-1)%6][1]) * abs(field[(idx_col+1)%6][1] - field[(idx_col-1)%6][1])
 
 
-print((big - small)*n)
+small_row = abs(arr[(idx_col+1)%6][1] - arr[(idx_col-1)%6][1])
+small_col = abs(arr[(idx_row+1)%6][1] - arr[(idx_row-1)%6][1])
+
+print((row * col - small_row * small_col) * n)
