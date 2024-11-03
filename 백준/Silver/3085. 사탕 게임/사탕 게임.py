343b69c99323593fc576069b5c8f91da51124fc2
Choi Hyuk Tae <40112123+htchoi1006@users.noreply.github.com>
2024-11-03 17:56:42 +0900

    [Silver II] Title: 사탕 게임, Time: 496 ms, Memory: 114908 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,49 +1,45 @@
-import sys
+from collections import deque
 
-input = sys.stdin.readline
+n = int(input())
+arr = [list(input()) for _ in range(n)]
 
-N = int(input())
-grid = [list(input().strip()) for i in range(N)]
+def check(arr):
+    maxnum = 1
+    for i in range(n):
+        cnt = 1
+        for j in range(n-1):
+            if arr[i][j] == arr[i][j+1]:
+                cnt += 1
+            else:
+                cnt = 1
+                
+            maxnum = max(maxnum, cnt)
+        
+        cnt = 1
+        for j in range(n-1):
+            if arr[j][i] == arr[j+1][i]:
+                cnt += 1
+            else:
+                cnt = 1
+                
+            maxnum = max(maxnum, cnt)
+            
+    return maxnum
+    
+        
 
-def find(idx, length, dir, cnt):
-    global visited
-    global answer
-    global grid
-    [r, c] = idx
-    tmp = grid[r][c]
-    nr, nc = r + d[dir][0], c + d[dir][1]
-    if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid) \
-       or visited[nr][nc]:
-        answer = max(length, answer)
-    else :
-        if grid[nr][nc] == tmp:
-            visited[nr][nc] = 1
-            find([nr, nc], length+1, dir, cnt)
-            visited[nr][nc] = 0
-        else :
-            if cnt == 1:
-                answer = max(length, answer)
-            else :
-                for dr, dc in d:
-                    fr, fc = nr+dr, nc+dc
-                    if fr < 0 or fr >= len(grid) or fc < 0 or fc >= len(grid) \
-                       or visited[fr][fc]:
-                        answer = max(length, answer)
-                    elif grid[fr][fc] == tmp:
-                        grid[fr][fc], grid[nr][nc] = grid[nr][nc], grid[fr][fc]
-                        visited[nr][nc] = 1
-                        find([nr, nc], length+1, dir, cnt+1)
-                        visited[nr][nc] = 0
-                        grid[fr][fc], grid[nr][nc] = grid[nr][nc], grid[fr][fc]
-                          
-answer = -sys.maxsize
-d = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-visited = [[0 for i in range(len(grid))]for j in range(len(grid))]
-
-for i in range(len(grid)):
-    for j in range(len(grid)):
-        for k in range(4):
-            visited[i][j] = 1
-            find([i, j], 1, k, 0)
-            visited[i][j] = 0
-print(answer)
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if j + 1 < n:
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+            ans = max(check(arr), ans)
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+            
+        if i + 1 < n:
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            ans = max(check(arr), ans)
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            
+print(ans)
+                
